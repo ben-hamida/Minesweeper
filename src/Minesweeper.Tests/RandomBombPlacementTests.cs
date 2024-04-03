c83f5ba2267ab4eea75799e21c56c9761ee4d802
Änis Ben Hamida <anis.benhamida@se.com>
2024-04-03 17:21:11 +0200

    Add tests for RandomBombPlacement

---
+++ src/Minesweeper.Tests/RandomBombPlacementTests.cs
@@ -0,0 +1,71 @@
+﻿namespace Minesweeper.Tests;
+
+public class RandomBombPlacementTests
+{
+    [Fact]
+    public void ShouldProduceValidCoordinates()
+    {
+        // Arrange
+        var bombPlacement = new RandomBombPlacement(5);
+        var gameCoordinates = GenerateGameCoordinates(5, 5);
+
+        // Test
+        var bombCoordinates = bombPlacement.GetBombCoordinates(gameCoordinates);
+
+        // Assert
+        Assert.True(bombCoordinates.IsSubsetOf(gameCoordinates));
+    }
+
+    [Fact]
+    public void ShouldProduceTheCorrectNumberOfBombCoordinates()
+    {
+        // Arrange
+        const int numberOfBombs = 8;
+        var bombPlacement = new RandomBombPlacement(numberOfBombs);
+        var gameCoordinates = GenerateGameCoordinates(7, 10);
+
+        // Test
+        var bombCoordinates = bombPlacement.GetBombCoordinates(gameCoordinates);
+
+        // Assert
+        Assert.Equal(numberOfBombs, bombCoordinates.Count);
+    }
+
+    [Fact]
+    public void ShouldProduceDifferentBombCoordinatesOnConsecutiveRuns()
+    {
+        // Arrange
+        var bombPlacement = new RandomBombPlacement(10, seed: 0);
+        var gameCoordinates = GenerateGameCoordinates(4, 4);
+
+        // Test
+        var firstBombCoordinates = bombPlacement.GetBombCoordinates(gameCoordinates);
+        var secondBombCoordinates = bombPlacement.GetBombCoordinates(gameCoordinates);
+
+        // Assert
+        Assert.NotEqual(firstBombCoordinates, secondBombCoordinates);
+    }
+
+    [Fact]
+    public void ShouldProduceSameCoordinatesOnConsecutiveRunsWhenUsingSameSeed()
+    {
+        // Arrange
+        const int seed = 4;
+        var bombPlacement1 = new RandomBombPlacement(10, seed);
+        var bombPlacement2 = new RandomBombPlacement(10, seed);
+        var gameCoordinates = GenerateGameCoordinates(8, 8);
+
+        // Test
+        var firstBombCoordinates = bombPlacement1.GetBombCoordinates(gameCoordinates);
+        var secondBombCoordinates = bombPlacement2.GetBombCoordinates(gameCoordinates);
+
+        // Assert
+        Assert.Equal(firstBombCoordinates, secondBombCoordinates);
+    }
+
+    private static HashSet<Coordinates> GenerateGameCoordinates(int width, int height) => (
+            from x in Enumerable.Range(0, width)
+            from y in Enumerable.Range(0, height)
+            select (x, y))
+        .ToHashSet();
+}
--- src/Minesweeper/RandomBombPlacement.cs
+++ src/Minesweeper/RandomBombPlacement.cs
@@ -2,17 +2,21 @@ namespace Minesweeper;
 
 public class RandomBombPlacement : IBombPlacement
 {
+    private readonly Random _random;
     private readonly int _numberOfBombs;
 
-    public RandomBombPlacement(int numberOfBombs) =>
+    public RandomBombPlacement(int numberOfBombs, int? seed = null)
+    {
+        _random = seed is null ? new Random() : new Random(seed.Value);
         _numberOfBombs = numberOfBombs > 0
             ? numberOfBombs
             : throw new ArgumentException("Number of bombs has to be a positive value", nameof(numberOfBombs));
+    }
 
     public IReadOnlySet<Coordinates> GetBombCoordinates(IReadOnlySet<Coordinates> gameCoordinates)
     {
         var bombCoordinatesCandidates = gameCoordinates.ToArray();
-        Random.Shared.Shuffle(bombCoordinatesCandidates);
+        _random.Shuffle(bombCoordinatesCandidates);
         return bombCoordinatesCandidates.Take(_numberOfBombs).ToHashSet();
     }
 }
